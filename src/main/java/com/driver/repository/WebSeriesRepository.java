42e418b0fab61785c38eda28a25c46a657c75a23
Abhijit_B <bandgarabhijitb@gamil.com>
2023-09-04 17:13:49 +0530

    First

--- src/main/java/com/driver/repository/WebSeriesRepository.java
+++ src/main/java/com/driver/repository/WebSeriesRepository.java
@@ -2,8 +2,14 @@ package com.driver.repository;
 
 import com.driver.model.WebSeries;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface WebSeriesRepository extends JpaRepository<WebSeries,Integer> {
 
     WebSeries findBySeriesName(String seriesName);
+
+//    @Query(value = "select * from webseries",nativeQuery = true)
+//    List<WebSeries> find();
 }
